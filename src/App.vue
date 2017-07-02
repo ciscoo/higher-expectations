8ed748293dd76e44b004a73ec87e7732d1749c5f
ciscoo <cisco21c@gmail.com>
2017-07-02 14:53:54 -0500

    Implement core layout.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,65 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+  <v-app>
+    <v-navigation-drawer persistent light :mini-variant.sync="mini" v-model="drawer">
+      <v-list class="pa-0">
+        <v-list-item>
+          <v-list-tile avatar tag="div">
+            <v-list-tile-avatar>
+              <img src="https://randomuser.me/api/portraits/men/85.jpg">
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>John Leider</v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn icon @click.native.stop="mini = !mini">
+                <v-icon>chevron_left</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list-item>
+      </v-list>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+        <v-list-item v-for="item in items" :key="item">
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed light>
+      <v-toolbar-side-icon light @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Higher Expectations</v-toolbar-title>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </main>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  data () {
+    return {
+      drawer: true,
+      items: [
+        { title: 'Dashboard', icon: 'dashboard' },
+        { title: 'Users', icon: 'supervisor_account' },
+        { title: 'Tasks', icon: 'assignment' },
+        { title: 'Groups', icon: 'group' },
+        { title: 'Events', icon: 'event' },
+        { title: 'Deadlines', icon: 'assignment_late' }
+      ],
+      mini: false,
+      right: null
+    }
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
