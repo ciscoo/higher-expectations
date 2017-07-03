ad034a569813391fc4f7d44fcbfe03a3d37c1453
ciscoo <cisco21c@gmail.com>
2017-07-02 21:33:53 -0500

    feat: add option routes.

--- server/routes/api-routes.js
+++ server/routes/api-routes.js
@@ -2,11 +2,13 @@ const express = require('express')
 const groupRoutes = require('./group-routes')
 const groupTypeRoutes = require('./group-type-routes')
 const eventRoutes = require('./event-routes')
+const optionRoutes = require('./option-routes')
 
 const router = express.Router()
 
 router.use('/groups', groupRoutes)
 router.use('/group-types', groupTypeRoutes)
 router.use('/events', eventRoutes)
+router.use('/option', optionRoutes)
 
 module.exports = router
