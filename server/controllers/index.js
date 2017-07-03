dc269929fe9d945f76090a71e21e47ad1cb69eba
ciscoo <cisco21c@gmail.com>
2017-07-02 21:26:21 -0500

    fix: add missing controllers.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,15 @@
 const groupController = require('./group-controller')
 const groupTypeController = require('./group-type-controller')
+const eventController = require('./event-controller')
+const optionController = require('./option-controller')
+const taskController = require('./task-controller')
+const userController = require('./user-controller')
 
 module.exports = {
   groupController,
-  groupTypeController
+  groupTypeController,
+  eventController,
+  optionController,
+  taskController,
+  userController
 }
