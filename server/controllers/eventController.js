4eebcaa0e12cd4abf5ca51d9f84eb72aaf130a9c
ciscoo <cisco21c@gmail.com>
2017-07-02 21:08:54 -0500

    chore: cleaned up and simplified event controller.

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -1,74 +1,28 @@
-// eventController.js
-
-const express = require('express')
-const router = express.Router()
-const bodyParser = require('body-parser')
-router.use(bodyParser.urlencoded({ extended: true }))
-
 const Events = require('../models/Event')
-const year = require('../utils/yearInSchool')
-const postPlans = require('../utils/postPlans')
-const extraInterest = require('../utils/extraInterests')
-const careerInterest = require('../utils/careerInterests')
 
-// CREATES A NEW EVENT
+exports.store = async (req, res) => {
+  const event = new Event(req.body)
+  await event.save()
+  res.json({ data: event })
+}
 
-router.post('/', function (req, res) {
-  Events.create({
-    startDate: req.body.startDate,
-    location: req.body.location,
-    startTime: req.body.startTime,
-    endTime: req.body.endTime,
-    title: req.body.title,
-    description: req.body.description,
-    contactInfo: req.body.contactInfo,
-    points: req.body.points,
-    validation: req.body.validation,
-    audience: year.NA,
-    district: req.body.district,
-    school: req.body.school,
-    postPlans: postPlans.NA,
-    extraInterest: extraInterest.NA,
-    careerInterest: careerInterest.NA
-  },
-   function (err, user) {
-     if (err) return res.status(500).send('There was a problem adding the information to the database.')
-     res.status(200).send(user)
-   })
-})
+exports.index = async (req, res) => {
+  const events = await Events.find().exec()
+  res.json({ data: events })
+}
 
-  // RETURNS ALL EVENTS IN THE DATABASE
-router.get('/', function (req, res) {
-  Events.find({}, function (err, users) {
-    if (err) return res.status(500).send('There was a problem finding the events.')
-    res.status(200).send(users)
-  })
-})
+exports.show = async (req, res) => {
+  const event = Events.findById(req.params.id).exec()
+  res.json({ data: event })
+}
 
-// GETS A SINGLE EVENT FROM THE DATABASE
-router.get('/:id', function (req, res) {
-  Events.findById(req.params.id, function (err, user) {
-    if (err) return res.status(500).send('There was a problem finding the event.')
-    if (!user) return res.status(404).send('No event found')
-    res.status(200).send(user)
-  })
-})
+exports.destroy = async (req, res) => {
+  await Events.findByIdAndRemove(req.params.id).exec()
+  res.status(201).send()
+}
 
-// DELETE A USER FROM THE DATABASE
-router.delete('/:id', function (req, res) {
-  Events.findByIdAndRemove(req.params.id, function (err, user) {
-    if (err) return res.status(500).send('There was a problem deleting the event.')
-    res.status(200).send('Event ' + user.name + ' was deleted.')
-  })
-})
-
-// UPDATES A SINGLE USER IN THE DATABASE
-router.put('/:id', function (req, res) {
-  Events.findByIdAndUpdate(req.params.id, req.body, {new: true},
-  function (err, user) {
-    if (err) return res.status(500).send('There was a problem updating the event.')
-    res.status(200).send(user)
-  })
-})
-
-module.exports = router
+exports.update = async (req, res) => {
+  await Events.findByIdAndUpdate(req.params.id, req.body)
+  const event = await Events.findById(req.params.id).exec()
+  res.json({ data: event })
+}
