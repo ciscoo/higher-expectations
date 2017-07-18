d885fad3463211a66076e03204f2c21678c01fa5
jordan anderson <ander332@rangers.uwp.edu>
2017-07-18 17:40:01 -0500

    Added the helper model to the index file

--- server/models/index.js
+++ server/models/index.js
@@ -3,11 +3,13 @@ const GroupType = require('./group-type-model')
 const Event = require('./event-model')
 const Task = require('./task-model')
 const User = require('./user-model')
+const Helper = require('./eventTaskHelper-model')
 
 module.exports = {
   Group,
   GroupType,
   Event,
   Task,
-  User
+  User,
+  Helper
 }
