a304a7c9247a3622301a58d7be8c6b70f4ed30cb
jordan anderson <ander332@rangers.uwp.edu>
2017-07-20 17:10:04 -0500

    Revert "updated task model referencing an outside model"
    This reverts commit b519768426c0cc9ee1719586635e58454c4b3ad2.

--- server/models/task-model.js
+++ server/models/task-model.js
@@ -1,12 +1,24 @@
 const mongoose = require('mongoose')
 
 const definition = {
+  name: String,
+  whatDescription: String,
+  whyDescription: String,
+  startDate: Date,
   endDate: Date,
   webLink: String,
-  type: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Helper'
-  }
+  pointOfContact: String,
+  points: Number,
+  validationType: String,
+  locationList: String,
+  requiremenList: String,
+  materialList: String,
+  audience: String,
+  district: String,
+  school: String,
+  postPlans: String,
+  extraInter: String,
+  careerInter: String
 }
 
 const taskSchema = new mongoose.Schema(definition)
