e85743bc8e8b4b52018cb5861232502258913325
jordan anderson <ander332@rangers.uwp.edu>
2017-07-20 17:11:46 -0500

    Revert "Added the helper model to the index file"
    This reverts commit d885fad3463211a66076e03204f2c21678c01fa5.

--- server/models/index.js
+++ server/models/index.js
@@ -3,13 +3,11 @@ const GroupType = require('./group-type-model')
 const Event = require('./event-model')
 const Task = require('./task-model')
 const User = require('./user-model')
-const Helper = require('./eventTaskHelper-model')
 
 module.exports = {
   Group,
   GroupType,
   Event,
   Task,
-  User,
-  Helper
+  User
 }
