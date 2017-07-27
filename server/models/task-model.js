6718f250689484c995216c350de75c35b7f716e6
jordan anderson <ander332@rangers.uwp.edu>
2017-07-27 14:47:31 -0500

    Updated task model with string arrays to be used for multiple selections.

--- server/models/task-model.js
+++ server/models/task-model.js
@@ -3,21 +3,19 @@ const mongoose = require('mongoose')
 const definition = {
   name: String,
   description: String,
-  startDate: Date,
-  endDate: Date,
+  dueDate: Date,
   webLink: String,
   location: String,
   contactInfo: String,
   points: Number,
-  validation: String,
   requirements: String,
   materialList: String,
   audience: String,
   district: String,
   school: String,
-  postPlans: String,
-  extraInter: String,
-  careerInter: String
+  postPlans: [String],
+  extraInter: [String],
+  careerInter: [String]
 }
 
 const taskSchema = new mongoose.Schema(definition)
