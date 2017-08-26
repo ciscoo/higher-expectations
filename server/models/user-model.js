b26edfe02891c035d4f2e5f61efc759fe8799fb1
jordan anderson <ander332@rangers.uwp.edu>
2017-08-26 17:56:27 -0500

    added time stamp fields to user model and also edited some arleady existing ones.

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,41 +1,54 @@
 const mongoose = require('mongoose')
 
 const definition = {
-  firstName: String,
-  lastName: String,
-  email: {type: String, unique: true},
-  google: String,
+  firstName: { type: String, trim: true, required: 'Missing required [firstName] property' },
+  lastName: { type: String, trim: true, required: 'Missing required [lastName] property' },
+  email: { type: String, trim: true, required: true },
+  // googleId: String,
   password: String,
-  tokens: Array,
-  counselor: String,
-  points: Number,
-  race: String,
-  ethnicity: String, /* list of constants defined in util folders */
-  district: [String],
-  school: [String],
-  yearInSchool: String,
-  postPlans: [String],
-  extraInter: [String],
-  careerInter: [String],
+  // tokens: Array,
+  counselor: { type: String, trim: true, default: 'You need to find your counselor' },
+  points: { type: Number, trim: true, default: 0 },
+  district: { type: [String], trim: true, required: true },
+  school: { type: [String], trim: true, required: true },
+  yearInSchool: { type: String, trim: true, required: true },
+  postPlans: { type: [String], trim: true, required: true },
+  extraInter: { type: [String], trim: true, required: true },
+  careerInter: { type: [String], trim: true, required: true },
   type: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Group'
-    // required: 'Missing required [type] property.'
+    ref: 'Group',
+    required: 'Missing required [type] property.'
   },
-  profile: {
-    name: String,
-    gender: String,
-    location: String,
-    website: String,
-    picture: String
-  },
+  // profile: {
+  //   name: String,
+  //   gender: String,
+  //   location: String,
+  //   website: String,
+  //   picture: String
+  // },
   tasks: [{
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Task',
-    unique: true
+    ref: 'Task'
+  }],
+  completedTasks: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Task'
+  }],
+  events: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Event'
+  }],
+  completedEvents: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Event'
   }]
 }
 
-const userSchema = new mongoose.Schema(definition)
+const options = {
+  timestamps: true
+}
+
+const userSchema = new mongoose.Schema(definition, options)
 
 module.exports = mongoose.model('User', userSchema)
