87759a9c230262e77659c90701cab158bc06f2d8
jordan anderson <ander332@rangers.uwp.edu>
2017-08-26 17:54:06 -0500

    added requested fields to the tasks model and added some data scrubbing

--- server/models/task-model.js
+++ server/models/task-model.js
@@ -1,23 +1,30 @@
 const mongoose = require('mongoose')
 
 const definition = {
-  name: String,
-  description: String,
-  dueDate: Date,
-  webLink: [String],
-  location: [String],
-  contactInfo: [String],
-  points: Number,
-  requirements: [String],
-  materialList: [String],
-  audience: [String],
-  district: [String],
-  school: [String],
-  postPlans: [String],
-  extraInter: [String],
-  careerInter: [String]
+  name: { type: String, required: true, trim: true },
+  description: { type: String, required: true, trim: true },
+  dueDate: { type: Date, required: true },
+  webLink: { type: [String], required: true, trim: true },
+  location: { type: [String], required: true, trim: true },
+  contactInfo: { type: [String], required: true, time: true },
+  points: { type: Number, required: true, trim: true },
+  requirements: { type: [String], required: true, trim: true },
+  materialList: { type: [String], required: true, trim: true },
+  audience: { type: [String], required: true, trim: true },
+  district: { type: [String], required: true, trim: true },
+  school: { type: [String], required: true, trim: true },
+  postPlans: { type: [String], required: true, trim: true },
+  extraInter: { type: [String], required: true, trim: true },
+  careerInter: { type: [String], required: true, trim: true },
+  verificationType: { type: Number, required: true, trim: true },
+  completionStatus: { type: Number, trim: true, default: null },
+  completedOn: { type: Date, default: null }
 }
 
-const taskSchema = new mongoose.Schema(definition)
+const options = {
+  timestamps: true
+}
+
+const taskSchema = new mongoose.Schema(definition, options)
 
 module.exports = mongoose.model('Task', taskSchema)
