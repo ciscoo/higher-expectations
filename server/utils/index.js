b385aa909b6482b6d12e51f87a3c314c0a48b3f6
jordan anderson <ander332@rangers.uwp.edu>
2017-08-26 18:00:12 -0500

    added a index.js file to the utils folder that can be required to access all lists of items.

---
+++ server/utils/index.js
@@ -0,0 +1,11 @@
+const careerInterests = require('./careerInterests')
+const postHighPlans = require('./postPlans')
+const externalInterests = require('./extraInterests')
+const yearInSchool = require('./yearInSChool')
+
+module.exports = {
+  careerInterests,
+  postHighPlans,
+  externalInterests,
+  yearInSchool
+}
